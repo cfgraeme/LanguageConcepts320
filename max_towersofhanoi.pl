2d5af24539fb243187cea05a20522cc353970d4e
Marge Marshall <mmmarsha1@g.cofc.edu>
2016-11-13 23:21:20 -0500

    second part of prolog assignment

---
+++ max_towersofhanoi.pl
@@ -0,0 +1,27 @@
+max([],0).
+max(List,M):- List = [M|Tail], max(Tail, N), M>N.
+max(List,M):- List =[N|Tail], max(Tail, M), M>N.
+
+hanoi(0,[]).
+hanoi(1,[[1,1,3]]).
+hanoi(1,[[1,3,2]]).
+hanoi(1,[[1,1,2]]).
+hanoi(1,[[1,2,3]]).
+hanoi(1,[[1,2,1]]).
+hanoi(1,[[1,3,1]]).
+
+hanoi(N,ML):- N>1, 0 is N mod 2, N1 is N-1, hanoi(N1, ML1), hanoi(N1, ML3),
+	ML1 = [M1|ML1Tail], M1 = [1,A,C],
+	ML2 = [M|ML3], M = [N,A,B],
+	ML3 = [M3|ML3Tail], M3 = [1,C,B],
+	A\=B, B\=C, C\=A,
+	append(ML1, ML2, ML).
+
+
+hanoi(N,ML):- N>1, 1 is N mod 2, N1 is N-1, hanoi(N1, ML1), hanoi(N1, ML3),
+	ML1 = [M1|ML1Tail], M1 = [1,A,C],
+	ML2 = [M|ML3], M = [N,A,C],
+	ML3 = [M3|ML3Tail], M3 = [1,B,A],
+	A\=B, B\=C, C\=A,
+	append(ML1, ML2, ML).
+
